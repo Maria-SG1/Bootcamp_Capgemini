d89326c811ab2bf5df33b9e5bc537742861ef193
MariaSG <mariya.golemanova@capgemini.com>
2025-03-11 13:51:45 +0100

    Gilded rose

---
+++ demo/src/main/java/com/example/util/Persona.java
@@ -0,0 +1,22 @@
+package com.example.util;
+
+public class Persona {
+	public int id = 1;
+	public String nombre = "Pepe";
+	public String apellido = "Pepes";
+	
+	
+	public Persona(int id, String nombre, String apellido) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+//	public Persona(int id, String nombre) {
+//		
+//	}
+	
+	
+
+}
